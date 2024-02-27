23ca1176f93210eaa7baf8211d28e1a101ccb5f1
[Urgyen Tsering] <[urgentsering77@gmail.com]>
2024-02-27 18:15:41 +0545

    Revert "Name of brands has changed into Shopper from shoppers in navbar"
    This reverts commit 8a020daa1c2270c87505ba6f52d7c0ec3058daf4.

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
       <div className="navbar">
         <div className="nav-logo">
           <img src={logo} alt="nav-logo" />
-          <p>Shoppers</p>
+          <p>Shopper</p>
         </div>
         <ul className="nav-menu">
           <li onClick={()=>{setMenu("shop");}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>} </li> {/* A React Router Link component that creates a link to the root path ("/"). */}
